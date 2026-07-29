96697d78a886a5b6a730b5fb6cc26b306e6d391f
ms-arch <79557881+ms-morrowvis@users.noreply.github.com>
2026-07-29 10:55:57 +1000

    Burger icon

--- nav.js
+++ nav.js
@@ -30,7 +30,7 @@
                         '</ul>' +
                     '</li>' +
                 '</ul>' +
-                '<a href="javascript:void(0);" class="nav-toggle" id="navToggle"><i class="fa fa-bars"></i></a>' +
+                '<a href="javascript:void(0);" class="nav-toggle" id="navToggle" aria-label="Menu"><svg viewBox="0 0 24 24" width="26" height="26" aria-hidden="true" focusable="false"><path d="M3 6h18M3 12h18M3 18h18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg></a>' +
             '</div>' +
         '</nav>';
 
