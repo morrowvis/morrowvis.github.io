9afa96fde2924fa96f5434d2ec6029d189309127
ms-arch <79557881+ms-morrowvis@users.noreply.github.com>
2026-09-18 13:54:47 +1000

    Rearranging

--- documentation/goals/goals-data.js
+++ documentation/goals/goals-data.js
@@ -23,11 +23,6 @@ const GOALS_MAIN = [
   "Inform and intersect with architectural work, as part of a wider vision of presenting and exploring spaces, imagined or built, at different scales."
 ];
 
-const GOALS_HINT = [
-  "Morrowind Visualisation Project is not a remaster. It will not aim to be playable other than exploration.",
-  "Morrowind Visualisation Project is not associated with Tamriel Rebuilt or Project Tamriel and no material from this project is to be taken as official promotion material."
-];
-
 const GOALS_SECTIONS = [
   {
     title: "Export Cells",
